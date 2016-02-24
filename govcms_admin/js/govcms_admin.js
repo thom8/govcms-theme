5f0f6a36e242cf1fd6c6dffba5043d89dc8146e0
paulkilla <paul.killer@gmail.com>
2016-02-24 15:36:22 +1100

    move to subfolder themes to make way for admin theme

---
+++ govcms_admin/js/govcms_admin.js
@@ -0,0 +1,17 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+var Drupal = Drupal || {};
+
+(function($, Drupal){
+    jQuery(document).ready(function($) {
+      CKEDITOR.dtd.a.h2 = 1;
+      CKEDITOR.dtd.a.div = 1;
+    });
+
+})(jQuery, Drupal);
+
+
+
