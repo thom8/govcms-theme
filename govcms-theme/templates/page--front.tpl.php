d83077d96b51239fdfae9e2c8e7fdc1493dbf764
paulkilla <paul.killer@gmail.com>
2016-02-25 11:17:56 +1100

    fix up front page

--- govcms-theme/templates/page--front.tpl.php
+++ govcms-theme/templates/page--front.tpl.php
@@ -1,5 +1,16 @@
 <div id="top-and-first-wrapper">
   <?php include "includes/header.tpl.php"; ?>
+  <?php if ($title): ?>
+    <section class="about" id="about">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-12 text-center">
+            <h1 class="title" id="page-title"><?php print $title; ?></h1>
+          </div>
+        </div>
+      </div>
+    </section>
+  <?php endif; ?>
 </div>
 
       <!-- #messages-console -->
