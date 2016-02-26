abe06d31dbdccea552fc45da053e1eb578f9435c
paulkilla <paul.killer@gmail.com>
2016-02-26 14:34:58 +1100

    tidy up news page

--- govcms-theme/templates/panels-twocol-stacked--node-govcms-news.tpl.php
+++ govcms-theme/templates/panels-twocol-stacked--node-govcms-news.tpl.php
@@ -18,7 +18,7 @@
 ?>
 
   <?php if ($content['top']): ?>
-    <section class="news-item-header">
+    <section class="news-item-header move-to-top">
       <div class="container">
         <div class="row">
           <div class="col-md-10 col-md-offset-1 text-center">
