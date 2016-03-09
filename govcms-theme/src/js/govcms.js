845d0af78e36120eef0efdbff9671049eb66267c
paulkilla <paul.killer@gmail.com>
2016-03-09 11:07:32 +1100

    fade in counter

--- govcms-theme/src/js/govcms.js
+++ govcms-theme/src/js/govcms.js
@@ -50,7 +50,7 @@ jQuery(document).ready(function() {
     });
 
     jQuery('div.region-content [class^="icon-"], div.region-content .fancyCounter').each(function () {
-        jQuery(this).addClass("fade")
+        jQuery(this).addClass("do-fade")
         var isInView = isElementVisible(jQuery(this));
         if(isInView && !jQuery(this).hasClass('faded')){
             jQuery(this).addClass('faded');
