99cdee79051f6baf227607b8da0ce5f01f6b437f
Tim Hurley <tim@timhurley.net>
2016-04-05 11:46:35 +1000

    updated text on pricing template

--- govcms-theme/panels-layouts/all-sites/govcms-layouts-all-sites.tpl.php
+++ govcms-theme/panels-layouts/all-sites/govcms-layouts-all-sites.tpl.php
@@ -24,7 +24,7 @@
     <div class="container">
       <div class="row text-center">
         <div class="col-md-10 col-md-offset-1">
-          <h2>Who is using govCMS?</h2>
+          <h2>Statistics</h2>
           <div class="row">
             <?php print $content['whos_using']; ?>
           </div>
