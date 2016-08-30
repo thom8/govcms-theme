b3d20c9a135856354a8f5c042196c7a878d06796
paulkilla <paul.killer@gmail.com>
2016-08-30 13:49:13 +1000

    changes

--- govcms-docs/template.php
+++ govcms-docs/template.php
@@ -1,3 +1,29 @@
 <?php
 
 
+function govcmsdocs_js_alter(&$javascript) {
+  $node_admin_paths = array(
+    'node/*/edit',
+    'node/add',
+    'node/add/*',
+  );
+  $replace_jquery = TRUE;
+  if (path_is_admin(current_path())) {
+    $replace_jquery = FALSE;
+  }
+  else {
+    foreach ($node_admin_paths as $node_admin_path) {
+      if (drupal_match_path(current_path(), $node_admin_path)) {
+        $replace_jquery = FALSE;
+      }
+    }
+  }
+  // Swap out jQuery to use an updated version of the library.
+  if ($replace_jquery) {
+    $javascript['misc/jquery.js']['data'] = '//cdnjs.cloudflare.com/ajax/libs/jquery/2.2.3/jquery.min.js';
+  }
+}
+
+function govcmsdocs_css_alter(&$css) {
+  unset($css[drupal_get_path('module','system').'/system.theme.css']);
+}
