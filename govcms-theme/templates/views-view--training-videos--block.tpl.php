1f699804b136706ca8fee1d01f9a89cb0f38a3f9
Tim Hurley <tim.hurley@finance.gov.au>
2016-11-23 14:13:30 +1100

    🎨💄 training video wrapping divs
    for layout fix

--- govcms-theme/templates/views-view--training-videos--block.tpl.php
+++ govcms-theme/templates/views-view--training-videos--block.tpl.php
@@ -27,62 +27,66 @@
  * @ingroup views_templates
  */
 ?>
-<div class="sortableList">
-  <?php print render($title_prefix); ?>
-  <?php if ($title): ?>
-    <?php print $title; ?>
-  <?php endif; ?>
-  <?php print render($title_suffix); ?>
-  <?php if ($header): ?>
-    <div class="view-header">
-      <?php print $header; ?>
+<div class="row">
+  <div class="col-md-10 col-md-offset-2">
+    <div class="sortableList">
+      <?php print render($title_prefix); ?>
+      <?php if ($title): ?>
+        <?php print $title; ?>
+      <?php endif; ?>
+      <?php print render($title_suffix); ?>
+      <?php if ($header): ?>
+        <div class="view-header">
+          <?php print $header; ?>
+        </div>
+      <?php endif; ?>
+
+      <?php if ($exposed): ?>
+        <div class="view-filters">
+          <?php print $exposed; ?>
+        </div>
+      <?php endif; ?>
+
+      <?php if ($attachment_before): ?>
+        <div class="attachment attachment-before">
+          <?php print $attachment_before; ?>
+        </div>
+      <?php endif; ?>
+
+      <?php if ($rows): ?>
+        <?php print $rows; ?>
+      <?php elseif ($empty): ?>
+        <div class="view-empty">
+          <?php print $empty; ?>
+        </div>
+      <?php endif; ?>
+
+      <?php if ($pager): ?>
+        <?php print $pager; ?>
+      <?php endif; ?>
+
+      <?php if ($attachment_after): ?>
+        <div class="attachment attachment-after">
+          <?php print $attachment_after; ?>
+        </div>
+      <?php endif; ?>
+
+      <?php if ($more): ?>
+        <?php print $more; ?>
+      <?php endif; ?>
+
+      <?php if ($footer): ?>
+        <div class="view-footer">
+          <?php print $footer; ?>
+        </div>
+      <?php endif; ?>
+
+      <?php if ($feed_icon): ?>
+        <div class="feed-icon">
+          <?php print $feed_icon; ?>
+        </div>
+      <?php endif; ?>
+    <?php /* class view */ ?>
     </div>
-  <?php endif; ?>
-
-  <?php if ($exposed): ?>
-    <div class="view-filters">
-      <?php print $exposed; ?>
-    </div>
-  <?php endif; ?>
-
-  <?php if ($attachment_before): ?>
-    <div class="attachment attachment-before">
-      <?php print $attachment_before; ?>
-    </div>
-  <?php endif; ?>
-
-  <?php if ($rows): ?>
-    <?php print $rows; ?>
-  <?php elseif ($empty): ?>
-    <div class="view-empty">
-      <?php print $empty; ?>
-    </div>
-  <?php endif; ?>
-
-  <?php if ($pager): ?>
-    <?php print $pager; ?>
-  <?php endif; ?>
-
-  <?php if ($attachment_after): ?>
-    <div class="attachment attachment-after">
-      <?php print $attachment_after; ?>
-    </div>
-  <?php endif; ?>
-
-  <?php if ($more): ?>
-    <?php print $more; ?>
-  <?php endif; ?>
-
-  <?php if ($footer): ?>
-    <div class="view-footer">
-      <?php print $footer; ?>
-    </div>
-  <?php endif; ?>
-
-  <?php if ($feed_icon): ?>
-    <div class="feed-icon">
-      <?php print $feed_icon; ?>
-    </div>
-  <?php endif; ?>
-<?php /* class view */ ?>
+  </div>
 </div>
