320b55bb6cd6a1fb51fbc9b0335731a3ece9d62a
Tim Hurley <tim.hurley@finance.gov.au>
2016-11-29 16:18:42 +1100

    💄✏️🎨🖼 All of the things
    💄 Improved hamburger menu styles for tablet-ish sized screens
    ✏️ Updated footer to link to community instead of RSS (using one of
    above icons)
    🎨 Tidied up unused classes on footer icons
    🖼 Added more to font icons

--- govcms-theme/templates/includes/footer.tpl.php
+++ govcms-theme/templates/includes/footer.tpl.php
@@ -4,16 +4,19 @@
       <div class="col-md-10 col-md-offset-1 text-center">
         <ul class="list-inline social">
           <li>
-            <a href="https://www.youtube.com/channel/UCpatbWRJ4qFghl0PxR75Y3w"><i class="icon-youtube-play fa-fw fa-3x"></i><span class="sr-only">YouTube</span></a>
+            <a href="https://www.youtube.com/channel/UCpatbWRJ4qFghl0PxR75Y3w"><i class="icon-youtube-play"></i><span class="sr-only">YouTube</span></a>
           </li>
           <li>
-            <a href="https://twitter.com/AusGovCMS"><i class="icon-twitter fa-fw fa-3x"></i><span class="sr-only">Twitter</span></a>
+            <a href="https://twitter.com/AusGovCMS"><i class="icon-twitter"></i><span class="sr-only">Twitter</span></a>
           </li>
           <li>
-            <a href="/rss.xml"><i class="icon-rss-square fa-fw fa-3x"></i><span class="sr-only">RSS</span></a>
-          </li>
+            <a href="https://community.govcms.gov.au/"><i class="icon-community-bubble"></i><span class="sr-only">Community</span></a>
+          </li><?php /*
           <li>
-            <a href="https://github.com/govCMS/"><i class="icon-github-orig fa-fw fa-3x"></i><span class="sr-only">GitHub</span></a>
+            <a href="/rss.xml"><i class="icon-rss-square"></i><span class="sr-only">RSS</span></a>
+          </li> */ ?>
+          <li>
+            <a href="https://github.com/govCMS/"><i class="icon-github-orig"></i><span class="sr-only">GitHub</span></a>
           </li>
         </ul>
         <hr class="small">
